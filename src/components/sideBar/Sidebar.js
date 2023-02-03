4aa375d5577b587d6d5d9bc621d6db70810fb716
namthse01 <namthse01@gmail.com>
2023-02-04 01:56:43 +0700

    fix

--- src/components/sideBar/Sidebar.js
+++ src/components/sideBar/Sidebar.js
@@ -92,21 +92,19 @@ const Sidebar = () => {
                 <Navbar expand="lg">
                     <Nav className="nav flex-column" activeKey={location.pathname}>
                         <Nav.Item className="user-avatar">
-                            <Nav.Link as={Link} to="/userProfile">
-                                <Avatar
-                                    src={defaultUserAvatar
-                                        // !data?.imgURL || imgLoading
-                                        //     ? defaultUserAvatar
-                                        //     : data.imgURL
-                                    }
-                                    // onLoad={() => setImgLoading(false)}
-                                    sx={{
-                                        width: "150px",
-                                        height: "150px",
-                                        border: "1px solid #000000",
-                                    }}
-                                />
-                            </Nav.Link>
+                            <Avatar
+                                src={defaultUserAvatar
+                                    // !data?.imgURL || imgLoading
+                                    //     ? defaultUserAvatar
+                                    //     : data.imgURL
+                                }
+                                // onLoad={() => setImgLoading(false)}
+                                sx={{
+                                    width: "150px",
+                                    height: "150px",
+                                    border: "1px solid #000000",
+                                }}
+                            />
                         </Nav.Item>
 
                         <div
